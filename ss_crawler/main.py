49b164d3fda6a771ce1be98c0c15df5849f897f1
ahmadsalimi <ahsa9978@gmail.com>
2023-06-12 02:58:09 +0330

    No sandbox

--- ss_crawler/main.py
+++ ss_crawler/main.py
@@ -194,6 +194,7 @@ def main(args: argparse.Namespace):
     chrome_options = webdriver.ChromeOptions()
     prefs = {'download.default_directory': os.getcwd()}
     chrome_options.add_experimental_option('prefs', prefs)
+    chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
 
     caps = DesiredCapabilities().CHROME
