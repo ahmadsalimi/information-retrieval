47dc3a52584b139133cedeb080d024d99a4fc87b
ahmadsalimi <ahsa9978@gmail.com>
2023-06-12 03:35:13 +0330

    fix errors

--- ss_crawler/main.py
+++ ss_crawler/main.py
@@ -53,41 +53,6 @@ def retry(func=None, max_retries: int = 5):
     return decorator
 
 
-@retry
-def crawl_paper(id_: str) -> Paper:
-    url = url_from_id(id_)
-    response = session.get(url, headers={
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) ',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Language': 'ja,en-US;q=0.7,en;q=0.3',
-        'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'none',
-        'Sec-Fetch-User': '?1',
-        'Upgrade-Insecure-Requests': '1',
-    })
-    if response.status_code != 200:
-        raise Exception(f'Failed to crawl {url}, status code: {response.status_code}')
-    soup = bs4.BeautifulSoup(response.text, 'html.parser')
-    return Paper(
-        id=id_,
-        title=soup.find('meta', {'name': 'citation_title'})['content'],
-        abstract=soup.find('meta', {'name': 'description'})['content'],
-        publication_year=int(soup.find('meta', {'name': 'citation_publication_date'})['content']),
-        authors=[a.text for a in soup.find_all('meta', {'name': 'citation_author'})],
-        related_topics=next((li.text.split(', ')
-                             for li in soup.find_all('li', {'class': 'paper-meta-item'})
-                             if li.find('span') is None), []),
-        citation_count=int(soup.find_all('span', {'class': 'paper-nav__nav-label'})[0].text.split()[0]),
-        reference_count=int(soup.find_all('span', {'class': 'paper-nav__nav-label'})[1].text.split()[0]),
-        references=[div['data-paper-id']
-                    for div in soup.find('div', {'data-test-id': 'reference'})
-                    .find('div', {'class': 'citation-list__citations'})])
-
-
 def id_from_url(url: str) -> str:
     return url.split('/')[-1]
 
@@ -105,8 +70,8 @@ def print_call(func):
 
 
 class SemanticScholarCrawler:
-    citation_count_pattern = re.compile(r'(?P<count>\d+) Citations')
-    reference_count_pattern = re.compile(r'(?P<count>\d+) References')
+    citation_count_pattern = re.compile(r'(?P<count>[\d,]+) Citations')
+    reference_count_pattern = re.compile(r'(?P<count>[\d,]+) References')
 
     def __init__(self, driver: webdriver.Chrome, seed_urls: List[str]):
         self.driver = driver
@@ -120,7 +85,6 @@ class SemanticScholarCrawler:
     def get_next(self):
         id_ = self.queue.pop(0)
         paper = self.crawl_paper(id_)
-        print(f'crawled {paper}')
         self.papers.append(paper)
         self.queue.extend(paper.references)
 
@@ -171,15 +135,15 @@ class SemanticScholarCrawler:
 
     @property
     def current_citation_count(self) -> int:
-        return int(next(mo.group('count')
-                        for x in self.driver.find_elements(By.CSS_SELECTOR, 'span.paper-nav__nav-label')
-                        if (mo := self.citation_count_pattern.match(x.text))))
+        return int(next((mo.group('count')
+                         for x in self.driver.find_elements(By.CSS_SELECTOR, 'span.paper-nav__nav-label')
+                         if (mo := self.citation_count_pattern.match(x.text))), float('NaN')))
 
     @property
     def current_reference_count(self) -> int:
-        return int(next(mo.group('count')
+        return int(next((mo.group('count')
                         for x in self.driver.find_elements(By.CSS_SELECTOR, 'span.paper-nav__nav-label')
-                        if (mo := self.reference_count_pattern.match(x.text))))
+                        if (mo := self.reference_count_pattern.match(x.text))), float('NaN')))
 
     @property
     def current_references(self) -> List[str]:
